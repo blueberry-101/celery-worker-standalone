956420a36da738353ab8d62b5ec729da477ace56
Swatantra <102201952+blueberry-101@users.noreply.github.com>
2024-05-30 05:01:20 +0530

    Update worker.py

--- worker.py
+++ worker.py
@@ -2,4 +2,12 @@
 from tasks import app
 
 if __name__ == '__main__':
-    app.worker_main(['worker', '--loglevel=info', '-E'])
+    app.worker_main([
+        'worker',
+        '--loglevel=info',
+        '--without-gossip',
+        '--without-mingle',
+        '--without-heartbeat',
+        '-Ofair',
+        '--pool=solo'
+    ])
